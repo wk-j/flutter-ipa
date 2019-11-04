53263aebd9c4d4f7732d6eeb5947016787dfdc1f
wk-j <somnuk.wk@gmail.com>
2019-11-04 16:34:57 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## PIA
+
+Build `.ipa` (OS App Store Package)
+
+```bash
+flutter create \
+    -i swift \
+    -a kotlin \
+    -t app \
+    --project-name hello \
+    --org wk .
+
+sh build.sh
+```
---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+flutter clean
+
+flutter build ios --release
+
+xcodebuild -scheme Runner \
+    -destination generic/platform=iOS \
+    archive -archivePath ".temp/R.xcarchive" \
+    -workspace "ios/Runner.xcworkspace" \
+    -configuration Release \
+    ENABLE_BITCODE=NO
+
+xcodebuild -exportArchive \
+    -exportOptionsPlist "ios/Runner/Info.plist" \
+    -archivePath ".temp/R.xcarchive" \
+    -exportPath  ".temp/R.ipa" \
+    ENABLE_BITCODE=NO
